62aa41f98daa06a0ed579c654969024602e023a4
Rodolfo Lara <rodolfolara0303>
2022-04-04 07:26:44 -0300

    Adicionada funcionalidade de criar array aleatório

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,9 @@
+/**VARIAVEIS PADRAO */
 const container = document.querySelector('.container')
+let cont = 1;
+const arrayAutomatico =[];
 const arraydeCores = [];
 
-
-
-
-
-function iniciarJogo() {
-    const modal = document.querySelector('.modal')
-    const btnStart = document.querySelector('#inputName')
-    btnStart.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        const namePlayer = document.querySelector('.namePlayer').value
-
-        if (namePlayer === '') {
-            alert('Insira o seu nome antes de jogar!')
-        } else {
-            modal.classList.add('hide')
-            modal.classList.add('show')
-            const name = document.querySelector('.name').innerText = namePlayer;
-        }
-
-
-    })
-}
 
 // Criação do tabuleiro
 
@@ -46,7 +26,7 @@ function criarTabuleiro() {
     btnBlue.classList.add('button', 'Blue')
 
     const btnInit = document.createElement('div')
-    btnInit.classList.add('button', 'init')
+    btnInit.classList.add('init')
 
     //span
 
@@ -90,25 +70,123 @@ function criarTabuleiro() {
 
     modalBox.append(modalLabel, modalInput, modalBtn)
 
-
-
-
-
-
     container.append(btnGreen, btnYellow, btnRed, btnBlue, btnInit, modalBox)
     main.append(container)
+    iniciarJogo();
 
 
+}
+
+/** Iniciar jogo - Insere o nome e desativa o modal */
+
+function iniciarJogo() {
+    const modal = document.querySelector('.modal')
+    const btnStart = document.querySelector('#inputName')
+    btnStart.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const namePlayer = document.querySelector('.namePlayer').value
+        
+
+        if (namePlayer === '') {
+            alert('Insira o seu nome antes de jogar!')
+        } else {
+            modal.classList.add('hide')
+            modal.classList.add('show')
+            const name = document.querySelector('.name').innerText = namePlayer;
+            criarSequencia();
+        }
+
+    }) 
+}
+/**Gerar numero */
+function numeroAleatório(){
+    const number = Math.round(Math.random()*3)
+    return number;
 }
 
 
 
 
+/**Cria sequencia para o jogo */
+function criarSequencia(){
+    
+        let val = numeroAleatório();
+        console.log(val)
+        if(val == 0){
+            console.log('oi0')
+            animarQuandoClicar('Green')
+            arrayAutomatico.push('Green')
+            
+            
+        }else if(val == 1){
+            console.log('oi1')
+            animarQuandoClicar('Red')
+            arrayAutomatico.push('Red')
+            
+            
+        }else if(val == 2){
+            console.log('oi2')
+            animarQuandoClicar('Yellow')
+            arrayAutomatico.push('Yellow')
+            
+        }else {
+            console.log('oi3')
+            animarQuandoClicar('Blue')
+            arrayAutomatico.push('Blue')
+            
+        }
+        
+        
+    
 
-/**Gerar numero */
-function numeroAleatório(){
-    const number = Math.round(Math.random()*3)
-    return console.log(number);
+    setInterval(()=>{
+        verificaClique(arrayAutomatico,arraydeCores)
+    },(3000))
+}
+
+/**Adicionar e remover a animação do botão */
+
+function animarQuandoClicar(cor){
+    const btnClicado = document.querySelector(`.${cor}`)
+    console.log(btnClicado)
+    
+    btnClicado.classList.add(`animacao${cor}`)
+    setTimeout(()=>{
+        btnClicado.classList.remove(`animacao${cor}`)
+    },500)
+    
+
+}
+
+
+
+/**VERICAR O CLIQUE */
+
+
+function verificaClique(arrayAutomatico, arrayCores){
+    const spanPlacar = document.querySelector('.placar')
+    if(arrayAutomatico == ''){
+        criarSequencia();
+    }else {
+        const result = arrayAutomatico.every((el, i)=>{
+            el === arrayCores[i]
+        })
+        if(result == true){
+            cont++;
+            console.log('acertei')
+            
+            spanPlacar.innerText = cont;
+            criarSequencia();
+            //Função que atualiza o placar com a variavel cont
+    
+        }else if( result == false){
+            cont = 0;
+            spanPlacar.innerText = '0'
+
+        }
+    }
+    
 }
 
 
@@ -122,6 +200,7 @@ function adicionarEventoAosBotoes(){
         console.log(button)
         const colorButton = button.classList[1]
         arraydeCores.push(colorButton)
+        console.log(colorButton)
         animarQuandoClicar(colorButton,button)
 
        })
@@ -129,23 +208,12 @@ function adicionarEventoAosBotoes(){
 }
 
 
-/**Adicionar e remover a animação do botão */
 
-function animarQuandoClicar(cor,botao){
-    const btnClicado = document.querySelector(`.${cor}`)
-    
-    btnClicado.classList.add(`animacao${cor}`)
-    setTimeout(()=>{
-        btnClicado.classList.remove(`animacao${cor}`)
-    },500)
-    
-
-}
 
 
 
 
 
 criarTabuleiro();
-iniciarJogo();
 adicionarEventoAosBotoes();
+
