e2cd98f7c039b49f0397657376826a704c7f67a8
Rodolfo Lara <rodolfolara0303>
2022-04-06 17:28:55 -0300

    Entrega finalizada

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
-/**VARIAVEIS PADRAO */
-let cont = 0;
-let arrayAutomatico = [];
-let arraydeCores = [];
+
 
 
 // Criação do tabuleiro
@@ -77,7 +74,10 @@ function criarTabuleiro() {
 
 }
 
-
+/**VARIAVEIS PADRAO */
+let cont = 0;
+let jogadasPessoa = [];
+let jogadasMaquina = [];
 
 
 /** Iniciar jogo - Insere o nome e desativa o modal */
@@ -98,14 +98,143 @@ function iniciarJogo() {
             modal.classList.add('hide')
             modal.classList.add('show')
             const name = document.querySelector('.name').innerText = namePlayer;
+            gerarAnimacaoNoBotao();
+            adicionarEventosAosBotoes();
             
         }
 
     })
 }
 
+function gerarNumeroRandomico(max, min){
+    return Math.floor(Math.random() * (max-min)) + min;
+}
+
+
+/**ANIMAÇÃO */
+function animacao(botao, cor){
+    botao.classList.add(`animacao${cor}`)
+    setTimeout(()=>{
+        botao.classList.remove(`animacao${cor}`)
+    },1000)
+}
+
+/**ANIMAR BOTAO */
+
+function animarBotao(botao, cor){
+    setTimeout(()=>{
+        animacao(botao, cor)
+    },2000)
+}
+
+
+
+
+/** Gerar animacao Randomica */
+
+
+
+
+function gerarAnimacaoNoBotao() {
+    const numeroRandom = gerarNumeroRandomico(0, 4);
+
+    const btn = document.querySelectorAll('.button')[numeroRandom];
+    const corBotao = btn.classList[1].split('-')[2];
+
+    jogadasMaquina.push(btn)
+    
+    let contadorR = 0;
+
+    const intervaloAnimacao = setInterval(()=>{
+        if(jogadasMaquina.length > 0){
+            setTimeout(()=>{
+                if(contadorR < jogadasMaquina.length){
+
+                    const btnAtual = jogadasMaquina[contadorR]
+                    const corAtual = btnAtual.classList[1].split('-')[2];
+
+                    animarBotao(btnAtual, corAtual);
+
+                    contadorR++;
+                } else {
+                    contadorR = 0;
+                    clearInterval(intervaloAnimacao);
+                }
+            },500)
+        } else {
+            animarBotao(btn,corBotao)
+            clearInterval(intervaloAnimacao)
+        }
+    },500)
+}
+
+
+/**Adicionar evento aos botões */
+
+function adicionarEventosAosBotoes(){
+    const botoes = document.querySelectorAll('.button')
+
+    botoes.forEach((el)=>{
+        el.addEventListener('click',(e)=>{
+            const botaoClicado = e.target;
+            const corBotaoClicado = e.target.classList[1].split('-')[2];
+            console.log(botaoClicado)
+            jogadasPessoa.push(botaoClicado)
+            
+            
+
+            if(verificaPerda()) {
+                alert('Perdeu!')
+                const main = document.querySelector('#main')
+                main.innerHTML =''
+                cont = 0;
+                jogadasMaquina = [];
+                jogadasPessoa = [];
+                
+                setTimeout(()=>{
+                    criarTabuleiro();
+                },2000)
+
+            } else if( jogadasPessoa.length === jogadasMaquina.length){
+                jogadasPessoa =[];
+                gerarAnimacaoNoBotao()
+                console.log('Acertou!')
+                cont++;
+                let spanPlacar =  document.querySelector('.placar')
+                spanPlacar.innerText = cont;
+
+            }
+   
+        })
+    })
+    
+}
+
+
+
+
+
+/**Condição vitória */
+
+function verificaPerda(){
+    for(let i = 0; i <jogadasPessoa.length ; i++){
+        const botao = jogadasMaquina[i];
+
+        if(jogadasPessoa[i] !== botao){
+            //errou
+            return true;
+
+
+        }
+
+    }
+    return false;
+}
+
+
+
+
+
 
 
 criarTabuleiro();
-
-
