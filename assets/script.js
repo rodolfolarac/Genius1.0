18e4a4a967d67322de615c1472443c55fdfb156c
Rodolfo Lara <rodolfolara0303>
2022-04-06 08:45:00 -0300

    recriação de toda a logica

--- assets/script.js
+++ assets/script.js
@@ -13,16 +13,16 @@ function criarTabuleiro() {
     container.classList.add('container')
 
     const btnGreen = document.createElement('div')
-    btnGreen.classList.add('button', 'Green')
+    btnGreen.classList.add('button', 'botao--green')
 
     const btnYellow = document.createElement('div')
-    btnYellow.classList.add('button', 'Yellow')
+    btnYellow.classList.add('button', 'botao--yellow')
 
     const btnRed = document.createElement('div')
-    btnRed.classList.add('button', 'Red')
+    btnRed.classList.add('button', 'botao--red')
 
     const btnBlue = document.createElement('div')
-    btnBlue.classList.add('button', 'Blue')
+    btnBlue.classList.add('button', 'botao--blue')
 
     const btnInit = document.createElement('div')
     btnInit.classList.add('init')
@@ -78,20 +78,6 @@ function criarTabuleiro() {
 }
 
 
-/**Adicionar evento de acender o botão */
-
-function adicionarEventoAosBotoes() {
-    const botoes = document.querySelectorAll('.button')
-    botoes.forEach((el) => {
-        el.addEventListener('click', (event) => {
-            const button = event.target
-            const colorButton = button.classList[1]
-            arraydeCores.push(colorButton)
-            animarQuandoClicar(colorButton)
-
-        })
-    })
-}
 
 
 /** Iniciar jogo - Insere o nome e desativa o modal */
@@ -101,7 +87,7 @@ function iniciarJogo() {
     const btnStart = document.querySelector('#inputName')
     btnStart.addEventListener('click', (event) => {
         event.preventDefault();
-        adicionarEventoAosBotoes()
+        
 
         const namePlayer = document.querySelector('.namePlayer').value
 
@@ -112,182 +98,11 @@ function iniciarJogo() {
             modal.classList.add('hide')
             modal.classList.add('show')
             const name = document.querySelector('.name').innerText = namePlayer;
-            criarSequencia();
+            
         }
 
     })
 }
-/**Gerar numero */
-function numeroAleatório() {
-    const number = Math.round(Math.random() * 3)
-    return number;
-}
-
-
-
-
-/**Função que anima em sequencia  */
-
-
-
-function criarSequencia() {
-
-    let val = numeroAleatório();
-    console.log(val)
-
-
-    if (val == 0) {
-        arrayAutomatico.push('Green')
-        animarArray(arrayAutomatico)
-
-
-
-    } else if (val == 1) {
-        arrayAutomatico.push('Red')
-        animarArray(arrayAutomatico)
-
-
-
-    } else if (val == 2) {
-        arrayAutomatico.push('Yellow')
-        animarArray( arrayAutomatico)
-
-
-    } else {
-        arrayAutomatico.push('Blue')
-        animarArray(arrayAutomatico)
-        
-
-
-    }
-
-
-
-}
-
-
-function animarArray( arrayAutomatico){
-    console.log(arrayAutomatico)
-    
-    const intervalo = setInterval(()=>{
-
-        if(arrayAutomatico.length>0){
-            arrayAutomatico.forEach((el,i)=>{
-                const btnClicado = document.querySelector(`.${el}`)
-                setTimeout(()=>{
-                       
-                    btnClicado.classList.add(`animacao${el}`)
-                    
-                },i*1000)
-                    
-                
-                        
-                    
-                    
-                //mudar aqui
-        
-        
-                setTimeout(() => {
-                    btnClicado.classList.remove(`animacao${el}`)
-                },i*500)
-        
-        
-        
-            })
-        } else {
-            clearInterval //teste
-        }
-
-        
-    },)
-    
-    console.log('Array clicado:::'+arraydeCores)
-    console.log('Array Auto:::'+arrayAutomatico)
-
-    
-
-     verificaClique(arrayAutomatico, arraydeCores)
-    
-        
-        
-    
-        
-    
-}
-
-/**Adicionar e remover a animação do botão */
-
-function animarQuandoClicar(cor, arrayAutomatico) {
-    
-
-    const btnClicado = document.querySelector(`.${cor}`)
-
-    
-        
-            btnClicado.classList.add(`animacao${cor}`)
-            
-       
-
-        setTimeout(()=>{
-            btnClicado.classList.remove(`animacao${cor}`)
-        },500)
-
-}
-
-
-
-
-
-
-
-/**VERICAR O CLIQUE */
-
-
-function verificaClique(arrayAutomatico, arrayCores) {
-    const spanPlacar = document.querySelector('.placar')
-    const spanInfo = document.querySelector('.span__info')
-
-
-    const result = arrayAutomatico.every((el, i) => el === arrayCores[i])
-
-
-    if (result == true) {
-        cont++;
-        spanPlacar.innerText = cont;
-        arrayCores = [];
-
-
-
-       
-            criarSequencia();
-        
-
-
-
-    } else {
-
-        cont = 0;
-        spanPlacar.innerText = '0'
-        arrayCores = [];
-        arrayAutomatico = [];
-        spanInfo.innerText = 'Que pena, você perdeu'
-        setTimeout(()=>{
-            main.innerHTML = ''
-            criarTabuleiro();
-        },3000)
-    }
-
-
-
-
-}
-
-
-
-
-
-
-
 
 
 
